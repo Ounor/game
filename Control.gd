bec3e0ba818a8caef3142309bfa324c3d3c7a61e
pereladov.ad <aleksandr.pereladov@simbirsoft.com>
2024-07-21 22:18:40 +0500

    111

--- Control.gd
+++ Control.gd
@@ -1,10 +1,34 @@
-extends Control
+extends Node2D
+
+var camera_value = Vector2()
+var position_value = Vector2()
+
+var pressed = false
+
+var viewWidth
 
 
+
+print(viewWidth)
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var viewport_size = get_viewport_rect().size
+
+	viewWidth = get_viewport_rect().size
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if pressed: 
+		$camera.position = (camera_value + position_value) - ($camera.get_local_mouse_position())
+
+
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.is_pressed():
+			position_value = $camera.get_local_mouse_position()
+			camera_value = $camera.position
+			pressed = true
+		else:
+			pressed = false
+	
